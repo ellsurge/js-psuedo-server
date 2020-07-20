0d447588f401a0b8075ebab8fe8b012ad56878f2
ellsurge <iorsasesesugh@gmail.com>
2020-07-20 15:29:04 +0100

    got stuck in making file write

--- js/jps.js
+++ js/jps.js
@@ -1,7 +1,20 @@
 
 class DB{
-    mk($name){
-        MD5($name)
-        $DB_list = 
+    mk(name){
+        var h_name = MD5(name)
+        var list  = $.ajax({
+            url: '../db/db_list.json',
+            dataType: 'json',
+            type: 'get',
+            catch: 'false',
+            success: (data)=>{
+               if(!data.row.includes(h_name)){
+                //    console.log("worked")
+                
+               }
+
+            }
+            
+        })
     }
 }
